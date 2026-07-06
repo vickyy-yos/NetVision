6f8b76c052b17a84e5c64e71d837dd9d69de7d55
Vicky <vickysatya42@gmail.com>
2026-07-06 14:13:37 +0700

    Add Railway deployment config

--- netvision-backend/db.js
+++ netvision-backend/db.js
@@ -6,8 +6,10 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
-    connectionLimit: 10
+    connectionLimit: 10,
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool.promise();
